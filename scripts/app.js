870ac4a27106581ec33b58a98f6d5f95cf83e07d
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2023-07-27 16:43:34 +0200

    playername

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,14 @@
+let editedPlayer = 0
+
 const editPlayer1BtnElement = document.getElementById('edit-player-1-btn');
 const editPlayer2BtnElement = document.getElementById('edit-player-2-btn');
+const cancelConfigBtnElement = document.getElementById('cancel-config-btn');
 
 
 const playerConfigOverlayElement = document.getElementById('config-overlay');
 const backdropElement = document.getElementById('backdrop');
+const formElement = document.querySelector('form');
 
-
-const cancelConfigBtnElement = document.getElementById('cancel-config-btn');
 // const submitConfigBtnElement = document.getElementById('submit-config-btn');
 
 // const inputElement = document.getElementById('player-name');
@@ -19,6 +21,8 @@ cancelConfigBtnElement.addEventListener('click',closePlayerConfig);
 backdropElement.addEventListener('click', closePlayerConfig);
 
 // submitConfigBtnElement.addEventListener('click',namePlayerConfig);
+formElement.addEventListener('submit', savePlayerConfig);
 
+const errorsOutputElement = document.getElementById('error-message')
 
 console.dir(document)
--- scripts/config.js
+++ scripts/config.js
@@ -1,13 +1,34 @@
-function openPlayerConfig() {
+function openPlayerConfig(event) {
+    editedPlayer = +event.target.dataset.playerid;
     playerConfigOverlayElement.style.display = 'block';
     backdropElement.style.display = 'block';
+
 }
 
 function closePlayerConfig() {
     playerConfigOverlayElement.style.display = 'none';
     backdropElement.style.display = 'none';
+    errorsOutputElement.textContent = '';
+    formElement.firstElementChild.classList.remove('error');
 }
 
 // function namePlayerConfig() {
 //     player1NameElement.textContent = inputElement.textContent;
 // }
+
+function savePlayerConfig(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const enteredPlayername = formData.get('playername').trim(); /*trim transforme '    ' en '' , input vide*/
+
+    if (!enteredPlayername) {
+        errorsOutputElement.textContent = 'Please enter a valid name!';
+        event.target.firstElementChild.classList.add('error');
+        return;
+    }
+
+const updatedPLayerDataElement = document.getElementById('player-' + editedPlayer + '-data');
+updatedPLayerDataElement.children[1].textContent = enteredPlayername
+
+}
+
